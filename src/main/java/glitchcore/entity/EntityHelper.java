becc30044273949199517906f8b50a37eccb9f5a
Adubbz <Adubbz@users.noreply.github.com>
2018-01-02 11:11:01 +1100

    Abstracted spawn egg owners

--- src/main/java/glitchcore/entity/EntityHelper.java
+++ src/main/java/glitchcore/entity/EntityHelper.java
@@ -19,10 +19,15 @@ public class EntityHelper
         EntityRegistry.registerModEntity(entityClass, entityName, id, mod, trackingRange, updateFrequency, sendsVelocityUpdates);
     }
 
-
     public static void registerEgg(ResourceLocation location, int primary, int secondary)
     {
         Class<? extends Entity> entityClass = EntityList.NAME_TO_CLASS.get(location.getResourceDomain() + "." + location.getResourcePath());
         EntityRegistry.registerEgg(entityClass, primary, secondary);
+    }
+
+    public static String getEntityEggOwner(EntityList.EntityEggInfo info)
+    {
+        int i = info.spawnedID.indexOf(".");
+        return i == -1 ? "" : info.spawnedID.substring(0, i - 1);
     }
 }
